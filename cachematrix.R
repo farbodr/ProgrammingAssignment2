ecf88ffd42a2fdd730fe0741613de326af451153
Fred Rahmanian <fredr@acm.org>
2014-06-22 16:02:58 -0400

    created and tested methods

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function creates and returns a matrix that can be cached
+## in order to improve computing time. 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolvedmatrix <- function(solve) m <<- solve
+  getsolvedmatrix <- function() m
+  list(set = set, get = get,
+       setsolvedmatrix = setsolvedmatrix,
+       getsolvedmatrix = getsolvedmatrix)
 }
 
 
-## Write a short comment describing this function
+## this function solves (inverses) a matrix created in makeCacheMatrix()
+## if first checks to make sure the matrix has not been solved. If it is in the cache
+## it returns hence saving the calculation
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x=matrix(), ...) {
+    m <- x$getsolvedmatrix()
+    if(!is.null(m)) {
+      message("getting cached solved matrix")
+      return(m)
+    }
+    a_matrix <- x$get()
+    m <- solve(a_matrix, ...)
+    x$setsolvedmatrix(m)
+    m
 }
